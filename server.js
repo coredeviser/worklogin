ad39d3600c2f4febbdd16ef3f861935a715cee44
coredeviser <coredevise@gmail.com>
2015-10-22 10:08:09 +0300

    port changed to 80

--- server.js
+++ server.js
@@ -24,5 +24,5 @@
         });
 
     // listen (start app with node server.js) ======================================
-    app.listen(8080);
-    console.log("App listening on port 8080");
+    app.listen(80);
+    console.log("App listening on port 80");
